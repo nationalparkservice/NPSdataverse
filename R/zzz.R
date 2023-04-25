220533664a62a88cc830eba88a96a62683767880
Rob Baker <robert_baker@nps.gov>
2023-04-25 14:09:56 -0600

    add check for github package updates necessary

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
 .onAttach <- function(...) {
   # See if any packages are needed
   needed <- pkgs[!is_attached(pkgs)]
+
+  #check for updates to github repo packages:
+  .update_git_repos()
+
   # If no packages are needed, return
   if (length(needed) == 0) {
     return()
