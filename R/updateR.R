04202b64d0968a125a5eb2f6029481aec891c292
Rob Baker <robert_baker@nps.gov>
2023-07-10 13:11:50 -0600

    fix update message

--- R/updateR.R
+++ R/updateR.R
@@ -27,7 +27,7 @@
       "{.strong To update {cli::qty(length(old_pkgs))}th{?is/ese} {cli::qty(length(old_pkgs))}package{?s}, please run:\n}")
     cli::cat_line("detach_NPSdataverse()")
     cli::cat_line("devtools::install_github(\"nationalparkservice/", old_pkgs, "\")\n\n")
-    cli::cat_line("Then close R and Rstudio and re-open a it in a new session.")
+    cli::cat_line("Close R and Rstudio. Open a new R session and reload the NPSdataverse.")
     cli::cat_line()
   }
 }
