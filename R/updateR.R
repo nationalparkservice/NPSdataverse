7955cb6a79ae8384f20ebd9a1acaf8c25da65d66
Sarah Wright <sarah_wright@nps.gov>
2025-04-12 13:35:09 -0600

    Fix how we were checking for out of date github packages, and clean up the cli message about it

--- R/updateR.R
+++ R/updateR.R
@@ -6,43 +6,55 @@
 #' @keywords internal
 #'
 .update_git_repos <- function() {
-  git_pkgs <- c(
-    "NPSdataverse",
-    "QCkit",
-    "EMLeditor",
-    "DPchecker",
-    "NPSutils",
-    "EMLassemblyline"
+  git_pkgs <- tibble::tibble(
+    package = c(
+      "QCkit",
+      "EMLeditor",
+      "DPchecker",
+      "NPSutils",
+      "EMLassemblyline",
+      "NPSdataverse"
+    ),
+    repo = c(
+      "nationalparkservice",
+      "nationalparkservice",
+      "nationalparkservice",
+      "nationalparkservice",
+      "EDIorg",
+      "nationalparkservice"),
+    branch = c(
+      "master",
+      "main",
+      "main",
+      "master",
+      "main",
+      "main"
+    )
   )
 
-  pkg_update <- remotes::package_deps(git_pkgs, dependencies = c(
-    "Imports",
-    "Remotes",
-    "Suggests"
-  ))
+  git_pkgs$local_version <- lapply(git_pkgs$package, packageVersion)
+  git_pkgs$latest_version <- mapply(function(x, y, z) .latest_github_version(repo = x, repo_owner = y, branch = z), git_pkgs$package, git_pkgs$repo, git_pkgs$branch, USE.NAMES = FALSE, SIMPLIFY = FALSE)
+  git_pkgs$behind <- mapply(function(x, y) x < y, git_pkgs$local_version, git_pkgs$latest_version)
+  git_pkgs$ahead <- mapply(function(x, y) x > y, git_pkgs$local_version, git_pkgs$latest_version)
+  git_pkgs$local_version <- sapply(git_pkgs$local_version, as.character)
+  git_pkgs$latest_version <- sapply(git_pkgs$latest_version, as.character)
 
-  if (any(pkg_update$diff < 0)) {
-    old_pkgs <- NULL
-    old_users <- NULL
-    for (i in seq_along(pkg_update$diff)) {
-      if (pkg_update$diff[i] < 0) {
-        old_pkgs <- append(old_pkgs, pkg_update$package[i])
-        old_users <- append(old_users, pkg_update$remote[[i]]$username)
-      }
-    }
+  if (any(git_pkgs$behind)) {
+    old_pkgs <- git_pkgs[git_pkgs$behind, ]
+
     load_header <- cli::rule(
       left = cli::pluralize(
-        "The following {cli::qty(length(old_pkgs))}package{?s} {?is/are} out of date:\n"
+        "The following {cli::qty(length(old_pkgs$package))}package{?s} {?is/are} out of date:\n"
       )
     )
     packageStartupMessage(load_header)
-    .print_cust_package_deps(pkg_update)
+    packageStartupMessage(.print_cust_package_deps(old_pkgs))
     cli::cat_line()
     cli::cli_text(
-      "{.strong To update {cli::qty(length(old_pkgs))}th{?is/ese} {cli::qty(length(old_pkgs))}package{?s}, please run:\n}"
+      "{.strong To update {cli::qty(length(old_pkgs$package))}th{?is/ese} {cli::qty(length(old_pkgs$package))}package{?s}, please run:\n}"
     )
     cli::cat_line("detach_NPSdataverse()")
-    cli::cat_line("devtools::install_github(\"", old_users, "/", old_pkgs, "\")")
+    cli::cat_line("devtools::install_github(\"", old_pkgs$repo, "/", old_pkgs$package, "\")")
     cli::cat_line("\nClose R and Rstudio. Open a new R session and reload the NPSdataverse.")
     cli::cat_line()
   } else {
@@ -68,29 +80,16 @@
 #' @return printed text to console
 #' @keywords internal
 #'
-.print_cust_package_deps <- function(x, show_ok = FALSE, ...) {
-  class(x) <- "data.frame"
-  x$remote <- lapply(x$remote, format)
-  ahead <- x$diff > 0L
-  behind <- x$diff < 0L
-  same_ver <- x$diff == 0L
-  x$diff <- NULL
-  x[] <- lapply(x, remotes:::format_str, width = 12)
-  if (any(behind)) {
-    # cat("Needs update -----------------------------\n")
-    print(x[behind, , drop = FALSE], row.names = FALSE, right = FALSE)
-  }
-  if (any(ahead)) {
-    cat("Not on CRAN ----------------------------\n")
-    print(x[ahead, , drop = FALSE], row.names = FALSE, right = FALSE)
-  }
-  if (show_ok && any(same_ver)) {
-    cat("OK ---------------------------------------\n")
-    print(x[same_ver, , drop = FALSE],
-      row.names = FALSE,
-      right = FALSE
-    )
-  }
+.print_cust_package_deps <- function(pkgs) {
+
+  pkg_names <- paste0(cli::col_yellow(cli::symbol$warning), " ", cli::col_blue(pkgs$package))
+  vers <- paste0("(", pkgs$local_version, " ", cli::symbol$arrow_right, " ", pkgs$latest_version, ")")
+
+  packages <- paste(
+    cli::ansi_align(pkg_names, max(cli::ansi_nchar(pkg_names))),
+    vers, collapse = "\n")
+
+  return(packages)
 }
 
 #' Get the latest version of an R package that is on GitHub
--- R/zzz.R
+++ R/zzz.R
@@ -13,9 +13,6 @@
   # See if any packages are needed
   needed <- pkgs[!is_attached(pkgs)]
 
-  # check for updates to github repo packages:
-  # .update_git_repos()
-
   # If no packages are needed, return
   if (length(needed) == 0) {
     return()
